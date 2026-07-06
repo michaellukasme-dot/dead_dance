832c240b6d478b93e5681c84d9077a5124dbe07a
Michael <michael@Marys-MacBook-Air.local>
2026-07-06 09:31:09 -0400

    deploy 2026-07-06

---
+++ intl_bands_seed.js
@@ -0,0 +1,139 @@
+/* intl_bands_seed.js — International chapter roster (34 tribute bands), from gratefuldeadtributebands.com World list. Factual (name+location). Wire into the Bands tab + map region "International" in a later pass. */
+window.INTL_BANDS = [
+{
+"name": "Abilene",
+"loc": "Tel Aviv"
+},
+{
+"name": "AoxoToxoA",
+"loc": "Lucerne Switzerland"
+},
+{
+"name": "Bokomaru",
+"loc": "Montreal"
+},
+{
+"name": "Canadian Beauty",
+"loc": "Victoria BC"
+},
+{
+"name": "Caution Jam",
+"loc": "Canada"
+},
+{
+"name": "Cherry Garcia Band",
+"loc": "Toronto ON"
+},
+{
+"name": "China Cat Riders",
+"loc": "Kingston, Ontario"
+},
+{
+"name": "Cosmic Charlies",
+"loc": "England"
+},
+{
+"name": "Cosmic Finger",
+"loc": "Germany"
+},
+{
+"name": "Dead Mondays",
+"loc": "Crepaja Serbia"
+},
+{
+"name": "Dead Set Fun Nuggets",
+"loc": "Northern Rivers NSW"
+},
+{
+"name": "Dead Walking",
+"loc": "Yerevan, Armenia"
+},
+{
+"name": "Deadnecks",
+"loc": "Melbourne, Australia"
+},
+{
+"name": "Drop of Dew",
+"loc": "Zurich, Switzerland"
+},
+{
+"name": "Franklin's Tower",
+"loc": "UK"
+},
+{
+"name": "GD-BC",
+"loc": "British Columbia"
+},
+{
+"name": "Grateful Dudes",
+"loc": "Leicester, UK"
+},
+{
+"name": "Just a Little Light",
+"loc": "Fredericton New Brunswick"
+},
+{
+"name": "Mark T Band",
+"loc": "Toronto"
+},
+{
+"name": "Mars Hotel",
+"loc": "Toronto, ON"
+},
+{
+"name": "Odd Thongz",
+"loc": "Mornington Peninsula, Australia"
+},
+{
+"name": "Rosegarland",
+"loc": "Ontario, Canada"
+},
+{
+"name": "Sardine Head",
+"loc": "Japan"
+},
+{
+"name": "Shakedown Street Liverpool",
+"loc": "United Kingdom"
+},
+{
+"name": "The Burning Shore",
+"loc": "Barcelona Spain"
+},
+{
+"name": "The Elevators",
+"loc": "Modiin Israel"
+},
+{
+"name": "The Hardest Daze",
+"loc": "Israel"
+},
+{
+"name": "The Lightnin Way",
+"loc": "Edinburgh"
+},
+{
+"name": "The Terrapins UK",
+"loc": "England"
+},
+{
+"name": "Tokyo Warlocks",
+"loc": "Tokyo, Japan"
+},
+{
+"name": "Typical Daydream",
+"loc": "London UK"
+},
+{
+"name": "Whose Cat Is Dead",
+"loc": "Toronto ON"
+},
+{
+"name": "Wildflower Seed",
+"loc": "Fife, Scotland"
+},
+{
+"name": "Workingman's Deed",
+"loc": "Scotland"
+}
+];
